ff2932f372dbbe63ed9f6dacd26df34e871468b7
g9308370 <g9308370@hotmail.com>
2015-05-04 16:35:05 +0800

    Add Seeder
    Add seeder for message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder {
 		 $this->call('CourseSeeder');
 		 $this->call('StudentSeeder');
 		 $this->call('CourseStudentSeeder');
+		 $this->call('MessageSeeder');
 	}
 
 }
---
+++ database/seeds/MessageSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Message;
+
+class MessageSeeder extends Seeder {
+
+  public function run()
+  {
+    DB::table('messages')->delete();
+
+    
+      Message::create([
+
+      	 'course_id'		   	=> 		'1',
+      	 'student_id'       =>    '1',
+         'from'  		        => 		'+886900000000',
+         'to'               => 		'+886933333333',
+         'content'          => 		'親愛的家長您好，貴子弟測試A君已抵達元祺文理補習班，請家長放心',
+         'delivertype'   		=>		'deliver',
+
+        	    
+      ]);
+      Message::create([
+        
+         'course_id'        =>    '1',
+         'student_id'       =>    '2',
+         'from'             =>    '+886900000000',
+         'to'               =>    '+886999876543',
+         'content'          =>    '親愛的家長您好，貴子弟測試B君已抵達元祺文理補習班，請家長放心',
+         'delivertype'      =>    'fail',
+
+      ]);
+      Message::create([
+        
+         'course_id'        =>    '2',
+         'student_id'       =>    '3',
+         'from'             =>    '+886900000000',
+         'to'               =>    '+886999876543',
+         'content'          =>    '親愛的家長您好，貴子弟測試C君已抵達元祺文理補習班，請家長放心',
+         'delivertype'      =>    'deliver',
+
+      ]);
+  }
+
+}
