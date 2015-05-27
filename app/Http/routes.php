1f5379c207f070942913605f65d2b89ea2f26ccf
g9308370 <g9308370@hotmail.com>
2015-05-27 11:57:12 +0800

    edit subdomain

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,7 +33,7 @@ $router->resource('messagestate','SmsMessageController');
 $router->resource('setting','SmsSettingController');
 
 
-Route::group(['domain' => '{account}.s1.send2me.cc'], function()
+Route::group(['domain' => '{account}.send2me.cc'], function()
 {
 
     Route::get('/', function($account)
