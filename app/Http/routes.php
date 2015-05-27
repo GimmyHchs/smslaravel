4bcf7cef8d11b22ef160f7d8ea30a87bc5522663
g9308370 <g9308370@hotmail.com>
2015-05-27 11:08:14 +0800

    trace

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,7 +40,7 @@ Route::group(['domain' => '{account}.school-mynet.cloudapp.net'], function()
     {
     	Session::set('subdomain',$account);
     	DB::setDefaultConnection('mysql_subdomainusers');
-    	$users = DB::select('select * from users where domain = hchs',array(1));
+    	$users = DB::select('select * from users where domain ="hchs"',array(1));
     	//$results = DB::select('select * from users where domain = '.$account,null);
     	dd($users);
     	$dbname=$account;
