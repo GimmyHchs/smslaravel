8f991448f0f71117d5c95ce2c64020462e6ae6af
g9308370 <g9308370@hotmail.com>
2015-06-06 10:16:03 +0800

    fix telphone

--- app/Smsapi/testSender.php
+++ app/Smsapi/testSender.php
@@ -5,19 +5,19 @@ const API = "http://nov.mynet.com.tw:9090/api";
 const KEY = "763ecaea1dce82286624baec4d";
 const SECRET = "c4f8e535806b72b889205cec64";
 
-$from = "0987570225";
+$from = "0987654321";
 $to = [
-	"0987570225"
+	"0912345678"
 ];
 $message = "set time at 14:48";
-
 
 $sender = new Send2me\Sender(API, KEY, SECRET);
 
 $sender->from($from);
 $sender->to($to);
 $sender->content($message);
-// $sender->at("2015-4-23 14:47:00");
 
 $response = $sender->send();
+
+//dump response
 var_dump($response);
