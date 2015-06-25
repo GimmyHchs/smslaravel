dd33b4d372c413a9b8bf09aed7e0f1866d4a1216
g9308370 <g9308370@hotmail.com>
2015-06-25 16:30:39 +0800

    Add queue's thread

--- app/Smsapi/SmsSendThread.php
+++ app/Smsapi/SmsSendThread.php
@@ -1,44 +1,28 @@
-<?php namespace App\Commands;
+<?php namespace App\Smsapi;
 
-use App\Commands\Command;
-
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Bus\SelfHandling;
-use Illuminate\Contracts\Queue\ShouldBeQueued;
 
 use App\Student;
-use App\Smsapi\SmsSendThread;
 
 use Illuminate\Http\Request;
 use App\Smsapi\Sender;
 use App\Smsapi\SmsLumen;
 
+
 	const KEY = "b67b96136e34ccd7b42656cd25";
 	const SECRET = "d739d9c7015a93064aacff78c8";
 
-class SendSms extends Command implements SelfHandling, ShouldBeQueued {
+class SmsSendThread extends Thread {
 
-	use InteractsWithQueue, SerializesModels;
-
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
 	private $student;
 	public function __construct(Student $student)
 	{
 		//
 		$this->student=$student;
 	}
+ 	public function run(){
 
-
-	public function handle()
-	{
 		//using API Class  check info from the project folder app/Smsapi/SmsLumen.php and testSmsLumen.php
 		//使用API Class 詳細資訊請查閱本專案內的檔案 app/Smsapi/SmsLumen.php 跟 testSmsLumen.php
-		/*
 		date_default_timezone_set("Asia/Taipei");
 		$arrived_at=date("Y-m-d")." ".date("h:i:sa");
 		$sender = new SmsLumen(KEY, SECRET);
@@ -48,11 +32,8 @@ class SendSms extends Command implements SelfHandling, ShouldBeQueued {
 			]);
 
 		$sender->setMessage("親愛的家長您好!貴子弟".$this->student->name."已經到達學校，請家長放心!  ".$arrived_at);
-		$sender->send();*/
-		$sendthread=new SmsSendThread($this->student);
-		$sendthread->run();
-
-
+		$sender->send();
+		sleep(1);
 	}
 
 }
